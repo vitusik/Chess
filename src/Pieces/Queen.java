baeaaf04926ad0359123116236e857eeaad01125
vitusik <vitfro91@gmail.com>
2018-11-01 14:15:22 +0200

    Added Queen Class, no need for testing

---
+++ src/Pieces/Queen.java
@@ -0,0 +1,10 @@
+package Pieces;
+
+public class Queen extends  Piece {
+    public Queen(int x, int y, boolean p){
+        super(x, y, p);
+        allowed_moves.add(MoveType.HORIZONTAL);
+        allowed_moves.add(MoveType.VERTICAL);
+        allowed_moves.add(MoveType.DIAGONAL);
+    }
+}
