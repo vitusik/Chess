41765afe28c1429a8637d6a1ecf3b88d76cea76f
vitusik <vitfro91@gmail.com>
2018-11-01 14:19:07 +0200

    Added King Class, began implementing the move_check method override

---
+++ src/Pieces/King.java
@@ -0,0 +1,21 @@
+package Pieces;
+
+public class King extends Piece {
+    private boolean checked;
+    public King(int x, int y, boolean p){
+        super(x, y, p);
+        allowed_moves.add(MoveType.HORIZONTAL);
+        allowed_moves.add(MoveType.VERTICAL);
+        allowed_moves.add(MoveType.DIAGONAL);
+        checked = false;
+    }
+
+    @Override
+    public boolean move_check(int new_x_coord, int new_y_coord) {
+        /*
+            1. after each move check to see that the king is not threatened by an enemy piece
+            2. castling
+         */
+        return false;
+    }
+}
