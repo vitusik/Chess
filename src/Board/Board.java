a50799eca06f29e37172c3b02c9d88f14937512c
vitusik <vitfro91@gmail.com>
2018-11-15 16:30:55 +0200

    Added board initialization method, and overridden to string method to print the board.
    reverse white and black player boolean values, due to an index mistake

--- src/Board/Board.java
+++ src/Board/Board.java
@@ -1,6 +1,8 @@
 package Board;
 
 import Pieces.*;
+import java.util.ArrayList;
+
 
 public class Board {
     public static final int X_UPPER_BOUND = 8;
@@ -8,8 +10,8 @@ public class Board {
     public static final int X_LOWER_BOUND = 1;
     public static final int Y_LOWER_BOUND = 1;
 
-    public static final boolean WHITE = true;
-    public static final boolean BLACK = false;
+    public static final boolean WHITE = false;
+    public static final boolean BLACK = true;
 
     public static Piece[] board = new Piece[X_UPPER_BOUND * Y_UPPER_BOUND];
 
@@ -17,5 +19,40 @@ public class Board {
         for(int i = 0; i < Board.X_UPPER_BOUND * Board.Y_UPPER_BOUND ; i++){
             Board.board[i] = null;
         }
+    }
+
+    public static void board_init()
+    {
+        ArrayList<Piece> piece_array = PieceFactory.create_pieces();
+        for(Piece p : piece_array)
+        {
+            board[p.getX_coord() + p.getY_coord() * X_UPPER_BOUND] = p;
+        }
+    }
+
+    @Override
+    public String toString() {
+        String board_string = "";
+        board_string = board_string + (new String(new char [49]).replace("\0","*"));
+        board_string = board_string + "\n";
+        for(int y = 0; y < Y_UPPER_BOUND; y++)
+        {
+            board_string = board_string + "*";
+            for (int x = 0; x < X_UPPER_BOUND; x++)
+            {
+                Piece p = Board.board[x + y * X_UPPER_BOUND];
+                if(p != null)
+                {
+                    board_string = board_string + "  "+ p + " " + (p.toString().length() == 1? " ":"");
+                }
+                else
+                {
+                    board_string = board_string + "     ";
+                }
+                board_string = board_string + "*";
+            }
+            board_string = board_string + "\n" + (new String(new char [49]).replace("\0","*")) + "\n";
+        }
+        return board_string;
     }
 }
