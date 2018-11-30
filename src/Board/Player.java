753e21f9983c959fa64774c2810b358a2244b207
vitusik <vitfro91@gmail.com>
2018-11-30 18:33:22 +0200

    Created player class, which will hold for each player, a list of its pieces, xy coordinates of the king, and a boolean which will tell if the player's king is under threat.
    also added a method which checks for threats on a given coordinate

---
+++ src/Board/Player.java
@@ -0,0 +1,38 @@
+package Board;
+
+import Pieces.Piece;
+
+import java.util.ArrayList;
+
+public class Player {
+    private boolean color;
+    private ArrayList<Piece> piece_list;
+    private boolean checked;
+    private int king_x_coord;
+    private int king_y_coord;
+
+    public Player(boolean color) {
+        this.color = color;
+        this.checked = false;
+    }
+
+    public void setPiece_list(ArrayList<Piece> piece_list) {
+        this.piece_list = piece_list;
+    }
+
+    public ArrayList<Piece> getPiece_list(){
+        return this.piece_list;
+    }
+
+    public boolean is_under_threat(int x, int y, ArrayList<Piece> possible_threats){
+
+        for(Piece piece : possible_threats)
+        {
+            if(piece.move_check(x, y))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
