6dc6bcb0db1f6db29f139c382d3f202ca2b0a8a7
vitusik <vitfro91@gmail.com>
2018-11-30 18:29:29 +0200

    Added borders to the printed board

--- src/Board/Board.java
+++ src/Board/Board.java
@@ -14,8 +14,8 @@ public class Board {
     public static final boolean BLACK = true;
 
     public static Piece[] board = new Piece[X_UPPER_BOUND * Y_UPPER_BOUND];
-    private static ArrayList<Piece> white_piece_list = new ArrayList<>();
-    private static ArrayList<Piece> black_piece_list = new ArrayList<>();
+    public static Player white_player = new Player(WHITE);
+    public static Player black_player = new Player(BLACK);
 
     public static void clear_board(){
         for(int i = 0; i < Board.X_UPPER_BOUND * Board.Y_UPPER_BOUND ; i++){
@@ -24,11 +24,13 @@ public class Board {
     }
 
     public static void board_init() {
+        ArrayList<Piece> white_piece_list = new ArrayList<>();
+        ArrayList<Piece> black_piece_list = new ArrayList<>();
         ArrayList<Piece> piece_array = PieceFactory.create_pieces();
         for(Piece p : piece_array)
         {
             board[p.getX_coord() + p.getY_coord() * X_UPPER_BOUND] = p;
-            if(p.getPlayer())
+            if(p.getColor())
             {
                 black_piece_list.add(p);
             }
@@ -37,38 +39,40 @@ public class Board {
                 white_piece_list.add(p);
             }
         }
-    }
-
-    public static ArrayList<Piece> getWhite_piece_list() {
-        return white_piece_list;
-    }
-
-    public static ArrayList<Piece> getBlack_piece_list() {
-        return black_piece_list;
+        white_player.setPiece_list(white_piece_list);
+        black_player.setPiece_list(black_piece_list);
     }
 
     public static void print_board() {
+        int seperatorLen = 67;
         StringBuilder board_string = new StringBuilder();
-        board_string.append(new String(new char [49]).replace("\0","*"));
+        board_string.append(new String(new char [seperatorLen]).replace("\0","*"));
         board_string.append("\n");
         for(int y = 0; y < Y_UPPER_BOUND; y++)
         {
+            board_string.append(8-y).append(" ");
             board_string.append("*");
             for (int x = 0; x < X_UPPER_BOUND; x++)
             {
                 Piece p = Board.board[x + y * X_UPPER_BOUND];
                 if(p != null)
                 {
-                    board_string.append("  ").append(p).append(" ").append((p.toString().length() == 1? " ":""));
+                    board_string.append("  ").append(p).append(p.getColor()? "-B":"-W").append(" ").append((p.toString().length() == 1? " ":""));
                 }
                 else
                 {
-                    board_string.append("     ");
+                    board_string.append("       ");
                 }
                 board_string.append("*");
             }
-            board_string.append("\n").append((new String(new char [49]).replace("\0","*"))).append("\n");
+            board_string.append("\n").append((new String(new char [seperatorLen]).replace("\0","*"))).append("\n");
         }
+        board_string.append("  ");
+        for(char c = 'A'; c <= 'H';c++ )
+        {
+            board_string.append("*   ").append(c).append("   ");
+        }
+        board_string.append("*");
         System.out.println(board_string);
     }
 }
