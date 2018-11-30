8138942330096e18dcaf4da0005002af4f06a0b0
vitusik <vitfro91@gmail.com>
2018-11-30 18:30:54 +0200

    Began implementing the King class, move check, currently only supports simple move, no castling ability

--- src/Pieces/King.java
+++ src/Pieces/King.java
@@ -1,13 +1,13 @@
 package Pieces;
 
+import Board.Board;
+
 public class King extends Piece {
-    private boolean checked;
     public King(int x, int y, boolean p){
         super(x, y, p);
         allowed_moves.add(MoveType.HORIZONTAL);
         allowed_moves.add(MoveType.VERTICAL);
         allowed_moves.add(MoveType.DIAGONAL);
-        checked = false;
     }
 
     @Override
@@ -21,6 +21,18 @@ public class King extends Piece {
             1. after each move check to see that the king is not threatened by an enemy piece
             2. castling
          */
+        // The king is allowed to travel in any direction with a maximum distance of 1 from is current position
+        if(Math.abs(this.getX_coord() - new_x_coord) <= 1 && Math.abs(this.getY_coord() - new_y_coord) <= 1)
+        {
+            if(Board.board[new_x_coord + Board.X_UPPER_BOUND * new_y_coord] == null)
+            {
+                this.setIn_starting_pos(false);
+                return true;
+            }
+            return false;
+        }
         return false;
     }
+
+
 }
