b52875a30cf1c680cf61f63bde7d3f08cc2830f6
vitusik <vitfro91@gmail.com>
2018-11-30 21:58:15 +0200

    Created a move and update method for the king, incorporated it within the kings move check method

--- src/Pieces/King.java
+++ src/Pieces/King.java
@@ -2,6 +2,8 @@ package Pieces;
 
 import Board.*;
 
+import java.util.ArrayList;
+
 public class King extends Piece {
     public King(int x, int y, boolean p){
         super(x, y, p);
@@ -22,14 +24,15 @@ public class King extends Piece {
             2. castling
          */
         // The king is allowed to travel in any direction with a maximum distance of 1 from is current position
+        boolean valid_move = false;
         if(Math.abs(this.getX_coord() - new_x_coord) <= 1 && Math.abs(this.getY_coord() - new_y_coord) <= 1)
         {
             if(Board.board[new_x_coord + Board.X_UPPER_BOUND * new_y_coord] == null)
             {
                 this.setIn_starting_pos(false);
-                return true;
+                valid_move = true;
             }
-            return false;
+            else return false;
         }
         // a castling move
         if(this.getY_coord() == new_y_coord && (new_x_coord == 0 || new_x_coord == 7))
@@ -50,9 +53,32 @@ public class King extends Piece {
                         return false;
                     }
                 }
-                return true;
+                valid_move = true;
             }
         }
-        return false;
+        if (valid_move)
+        {
+            valid_move = this.king_make_move_and_update(new_x_coord, new_y_coord);
+        }
+        return valid_move;
+    }
+
+    boolean king_make_move_and_update(int new_x_coord, int new_y_coord) {
+        Piece piece_in_final_positon = Board.board[new_x_coord + Board.X_UPPER_BOUND * new_y_coord];
+        Board.board[new_x_coord + Board.X_UPPER_BOUND * new_y_coord] = this;
+        int cur_x = this.getX_coord();
+        int cur_y = this.getY_coord();
+        Board.board[cur_x + Board.X_UPPER_BOUND * cur_y] = null;
+        this.setXYcoord(new_x_coord, new_y_coord);
+        ArrayList<Piece> threats = this.getColor() ? Board.white_player.getPiece_list() : Board.black_player.getPiece_list();
+        if (Player.is_under_threat(new_x_coord, new_y_coord, threats)) {
+            this.setXYcoord(cur_x, cur_y);
+            Board.board[cur_x + Board.X_UPPER_BOUND * cur_y] = this;
+            Board.board[new_x_coord + Board.X_UPPER_BOUND * new_y_coord] = piece_in_final_positon;
+            return false;
+        }
+        if(this.getColor()) Board.black_player.setKing_x_y_coord(new_x_coord, new_y_coord);
+        else Board.white_player.setKing_x_y_coord(new_x_coord, new_y_coord);
+        return true;
     }
 }
