5f68a3d59da9448334f0adb11159d26b8b01f3e8
vitusik <vitfro91@gmail.com>
2018-12-02 19:29:57 +0200

    Added support for the condition of castling, that the king isn't threatened

--- src/Pieces/King.java
+++ src/Pieces/King.java
@@ -40,14 +40,16 @@ public class King extends Piece {
             /*
             the king and the rook has'nt moved, there is no need to check if the other piece is a rook
             or if the piece is the same color of the king, because if it isn't than it has obliviously moved*/
+            Player cur = this.getColor()? Board.black_player:Board.white_player;
+            // cant castle if king is checked
+            if (!cur.isChecked()) return false;
             int step = (new_x_coord > this.getX_coord())? 1 : -1;
             int rook_x_coord = step == 1? 7:0;
             if(this.isIn_starting_pos() && Board.board[rook_x_coord + Board.X_UPPER_BOUND * new_y_coord].isIn_starting_pos())
             {
-
+                Player enemy = this.getColor()? Board.white_player: Board.black_player;
                 for(int i = this.getX_coord() + step; i != new_x_coord + step; i += step)
                 {
-                    Player enemy = this.getColor()? Board.white_player: Board.black_player;
                     // first part checks that the path to the rook is empty
                     // second part checks that the path isn't threatened by the enemy player
                     if(Board.board[i + Board.X_UPPER_BOUND * new_y_coord] != null || Board.is_under_threat(i, new_y_coord, enemy.getPiece_list()))
