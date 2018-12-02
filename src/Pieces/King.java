30086bfff6b944ba36d3084b9649d31b8672cbf6
vitusik <vitfro91@gmail.com>
2018-12-02 19:31:20 +0200

    Minor change

--- src/Pieces/King.java
+++ src/Pieces/King.java
@@ -19,11 +19,6 @@ public class King extends Piece {
 
     @Override
     public boolean move_check(int new_x_coord, int new_y_coord) {
-        /*
-            1. after each move check to see that the king is not threatened by an enemy piece
-            2. castling
-         */
-        // The king is allowed to travel in any direction with a maximum distance of 1 from is current position
         boolean valid_move = false;
         if(Math.abs(this.getX_coord() - new_x_coord) <= 1 && Math.abs(this.getY_coord() - new_y_coord) <= 1)
         {
